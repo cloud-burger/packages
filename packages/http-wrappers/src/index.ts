a53250cf390301a7b33a9206c416ce720c4af264
Daniel Aguiar <daniel_aguiar_alves@hotmail.com>
2024-09-18 20:07:14 -0300

    feat: export functions

---
+++ packages/http-wrappers/src/index.ts
@@ -0,0 +1,7 @@
+export { get } from './get';
+export { patch } from './patch';
+export { post } from './post';
+export { put } from './put';
+export { remove } from './remove';
+export { AxiosRequest } from './types/request';
+export { AxiosResponse } from './types/response';
